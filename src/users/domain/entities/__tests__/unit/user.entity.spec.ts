61e55e094c30d30dd1ba8126d0d527697217251c
BritoRuan <ruan.desenvolvedor@gmail.com>
2025-10-30 21:06:07 -0300

    test: adding new unit tests for user entity

--- src/users/domain/entities/__tests__/unit/user.entity.spec.ts
+++ src/users/domain/entities/__tests__/unit/user.entity.spec.ts
@@ -21,4 +21,26 @@ describe('UserEntity unit tests', () => {
         expect(sut.props.password).toEqual(props.password)
         expect(sut.props.createdAt).toBeInstanceOf(Date)
     })
+
+    it('should return by getter of name field corrected', () => {
+        expect(sut.props.name).toBeDefined()
+        expect(sut.props.name).toEqual(props.name)
+        expect(typeof sut.props.name).toBe('string')
+    })
+
+    it('should return by getter of email corrected', () => {
+        expect(sut.props.email).toBeDefined()
+        expect(sut.props.email).toEqual(props.email)
+        expect(typeof sut.props.email).toBe('string')
+    })
+    it('should return by getter of password corrected', () => {
+        expect(sut.props.password).toBeDefined()
+        expect(sut.props.password).toEqual(props.password)
+        expect(typeof sut.props.password).toBe('string')
+    })
+
+    it('should return by getter of createdAt corrected', () => {
+        expect(sut.props.createdAt).toBeDefined()
+        expect(sut.props.createdAt).toBeInstanceOf(Date)
+    })
 })
