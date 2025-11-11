b588e5f183021cc6f6ef5b08e038a80446ff952e
BritoRuan <ruan.desenvolvedor@gmail.com>
2025-11-11 00:08:26 -0300

    test: adding new test of update name e password methods

--- src/users/domain/entities/users/user.entity.spec.ts
+++ src/users/domain/entities/users/user.entity.spec.ts
@@ -24,19 +24,47 @@ describe('UserEntity unit tests', () => {
         expect(typeof sut.props.name).toBe('string')
     })
 
+    it('should return by setter of name field corrected', () => {
+        sut['name'] = 'other name'
+        expect(sut.props.name).toEqual('other name')
+        expect(typeof sut.props.name).toBe('string')
+    })
+
     it('should return by getter of email corrected', () => {
         expect(sut.props.email).toBeDefined()
         expect(sut.props.email).toEqual(props.email)
         expect(typeof sut.props.email).toBe('string')
     })
+
     it('should return by getter of password corrected', () => {
         expect(sut.props.password).toBeDefined()
         expect(sut.props.password).toEqual(props.password)
         expect(typeof sut.props.password).toBe('string')
     })
 
+    it('should return by setter of password corrected', () => {
+        sut['password'] = 'other_password'
+        expect(sut.props.password).toEqual('other_password')
+        expect(typeof sut.props.password).toBe('string')
+    })
+
     it('should return by getter of createdAt corrected', () => {
         expect(sut.props.createdAt).toBeDefined()
         expect(sut.props.createdAt).toBeInstanceOf(Date)
+    })
+
+    it('should update a user', () => {
+        sut.update('maquinadinho')
+        expect(sut.props.name).toBeDefined()
+        expect(sut.props.name).toEqual('maquinadinho')
+        expect(sut.props.name).not.toEqual('ferret')
+        expect(typeof sut.props.name).toBe('string')
+    })
+
+    it('should update password of user', () => {
+        sut.updatePassword('8628774725754')
+        expect(sut.props.password).toBeDefined()
+        expect(sut.props.password).toEqual('8628774725754')
+        expect(sut.props.password).not.toEqual('coxinha123')
     })
 })
