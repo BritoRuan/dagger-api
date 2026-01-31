b732cce77d05fc6d08e9a9593e1a58d1102b5308
BritoRuan <ruan.desenvolvedor@gmail.com>
2026-01-31 03:44:09 -0300

    refactor: rename folder name in shared

--- src/shared/infrastructure/env-config/env-config.module.ts
+++
@@ -1,7 +0,0 @@
-import { Module } from '@nestjs/common';
-import { EnvConfigService } from './application/services/env-config.service';
-
-@Module({
-  providers: [EnvConfigService],
-})
-export class EnvConfigModule {}
