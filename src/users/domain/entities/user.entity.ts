351b1c8ce446bc60bbe9c2a67c8c0465f0878e93
BritoRuan <ruan.desenvolvedor@gmail.com>
2026-01-31 19:41:33 -0300

    feat: add user entity

--- src/users/domain/entities/user.entity.ts
+++ src/users/domain/entities/user.entity.ts
@@ -1,1 +1,44 @@
-export class UserEntity {}
+import { Entity } from '@/shared/domain/entities/entity';
+import { UserProps } from '../types/user-props.types';
+
+export class UserEntity extends Entity<UserProps> {
+  constructor(
+    public readonly props: UserProps,
+    id?: string,
+  ) {
+    super(props, id);
+    this.props.createdAt = this.props.createdAt ?? new Date();
+  }
+
+  get name() {
+    return this.props.name;
+  }
+
+  private set name(value: string) {
+    this.props.name = value;
+  }
+
+  get email() {
+    return this.props.email;
+  }
+
+  get password() {
+    return this.props.password;
+  }
+
+  private set password(value: string) {
+    this.props.password = value;
+  }
+
+  get createdAt() {
+    return this.props.createdAt;
+  }
+
+  update(value: string): void {
+    this.name = value;
+  }
+
+  updatePassword(value: string): void {
+    this.password = value;
+  }
+}
