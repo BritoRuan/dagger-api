b15b5f74df63933e239136e60291b35dfd5d3f92
BritoRuan <ruan.desenvolvedor@gmail.com>
2026-01-31 03:45:51 -0300

    feat: add root config to env config module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
-import { EnvConfigModule } from './shared/infrastructure/env-config/env-config.module';
+import { EnvConfigModule } from './shared/infra/env-config/env-config.module';
 
 @Module({
   imports: [ConfigModule, EnvConfigModule],
